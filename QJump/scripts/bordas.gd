f1228706b51e092e002ab32270402633bf7257a4
Andre Moreira <moreira.andre.p1@gmail.com>
2018-07-10 00:55:27 -0300

    bordas

--- QJump/scripts/bordas.gd
+++ QJump/scripts/bordas.gd
@@ -1,14 +1,20 @@
 extends Area2D
 
+var pre_qbit1 = preload("res://scenes/qbit.tscn")
+
 func _ready():
 	pass
 
 func _on_bordas_body_enter( body ):
 	if body.get_layer_mask() == 8 and game.teste == 1:
 		print("QBIT colidiu")
+		var qbit1 = pre_qbit1.instance()
+		get_owner().add_child(qbit1)
+		qbit1.set_pos(self.get_global_pos())
 		game.teste = 0
 		game.lifes -= 1
-		get_tree().reload_current_scene()
+		#get_tree().reload_current_scene()
+		get_tree().set_current_scene(qbit1)
 		if game.lifes <= 0:
 			get_tree().finish()
 	else:
