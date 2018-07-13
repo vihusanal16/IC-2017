f5e3555bb25c9396672ce7c51efee61faf9e876e
Andre Moreira <moreira.andre.p1@gmail.com>
2018-07-12 22:48:57 -0300

    tela inicial

---
+++ QJump/scripts/play.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready():
+	set_process(true)
+	pass
+	
+func _process(delta):
+	if Input.is_action_pressed("enter"):
+		get_tree().change_scene("res://scenes/main.tscn")
+
+
+func _on_TouchScreenButton_pressed():
+	get_tree().change_scene("res://scenes/main.tscn")
+	pass
