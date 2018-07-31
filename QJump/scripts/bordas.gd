c75f2e5a97403c0e9e69ef04c724749ac8252219
Vitor Alencar <vitor.alencar4@outlook.com>
2018-07-30 21:10:10 -0300

    Alterando a inicialização das bordas

--- QJump/scripts/bordas.gd
+++ QJump/scripts/bordas.gd
@@ -9,6 +9,7 @@ func _on_bordas_body_enter( body ):
 	if body.get_layer_mask() == 8 and game.teste == 1:
 		print("QBIT colidiu")
 		game.estado = game.MORTO
+		game.troca_comandos = false
 		var qbit1 = pre_qbit1.instance()
 		get_owner().add_child(qbit1)
 		qbit1.set_pos(self.get_global_pos())
@@ -17,6 +18,7 @@ func _on_bordas_body_enter( body ):
 		get_tree().reload_current_scene()
 		if game.lifes <= 0:
 			get_tree().change_scene("res://scenes/menu.tscn")
+			game.lifes = 3
 	else:
 		game.teste = 1
 		
